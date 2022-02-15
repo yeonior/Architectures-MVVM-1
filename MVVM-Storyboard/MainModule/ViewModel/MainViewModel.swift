4ce67782b25e8d849dbbe33b43ee9a635eb15d3f
Rus <de.mr44@gmail.com>
2022-02-15 20:21:26 +0500

    Added MainViewModel

---
+++ MVVM-Storyboard/MainModule/ViewModel/MainViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewModel.swift
+//  MVVM-Storyboard
+//
+//  Created by Ruslan on 15.02.2022.
+//
+
+import Foundation
+
+protocol MainViewModelProtocol {
+    var updateViewData: ((ViewData) -> ())? { get set }
+    func startFetch()
+}
+
+final class MainViewModel: MainViewModelProtocol {
+    
+    public var updateViewData: ((ViewData) -> ())?
+    
+    init() {
+        updateViewData?(.initialize)
+    }
+    
+    public func startFetch() {
+        
+        // start loading
+        updateViewData?(.loading(ViewData.Data(icon: nil,
+                                               title: nil,
+                                               description: nil,
+                                               phoneNumber: nil)))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+            self?.updateViewData?(.success(ViewData.Data(icon: "checkmark.diamond",
+                                                        title: "success",
+                                                        description: "everything is ok",
+                                                        phoneNumber: nil)))
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) { [weak self] in
+            self?.updateViewData?(.success(ViewData.Data(icon: "xmark.diamond",
+                                                        title: "failure",
+                                                        description: "something went wrong",
+                                                        phoneNumber: nil)))
+        }
+    }
+}
